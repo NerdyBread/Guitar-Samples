34c8922d7bfbab93d5c28b95e3ed3438fdde05d5
Sam F <samfondacaro2006@gmail.com>
2021-05-19 13:27:43 -0400

    Branching Test

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
 	SECRET_KEY = os.environ.get('SECRET_KEY') or open(os.path.join(os.path.dirname(__file__), 'config.key'), 'r').read()
 	# .key in gitignore
