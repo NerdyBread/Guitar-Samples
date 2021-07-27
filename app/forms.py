3094a2d50046dfd4ca5fa65b8b1b433eee329024
Sam F <samfondacaro2006@gmail.com>
2021-07-27 15:49:30 -0400

    I fucked up the merge conflict a little bit

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, FileField
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length
 from app.models import User
 
