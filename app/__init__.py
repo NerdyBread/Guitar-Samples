f57c3b52607733ff75c3ca8d36031d89987f39c0
Sam F <samfondacaro2006@gmail.com>
2021-07-27 01:10:49 -0400

    Got rid of the oauth thing

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,11 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from authlib.integrations.flask_client import OAuth
 
 app = Flask(__name__)
 
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-oauth = OAuth(app)
 
 from app import routes, models
--- app/forms.py
+++ app/forms.py
@@ -3,7 +3,8 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Passcode', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+	username = StringField('Username', validators=[DataRequired()])
+	email = StringField('Email', validators=[DataRequired()])
+	password = PasswordField('Passcode', validators=[DataRequired()])
+	remember_me = BooleanField('Remember Me')
+	submit = SubmitField('Sign In')
